78f1d04dad70fe67b200f90ac4b48e3df98871a7
Joanie Lessnick <joanie.lessnick@gmail.com>
2021-12-10 16:21:47 -0500

    Small typo and css fix

--- src/LabelMaker/PrivacyPolicyTermsConditions.js
+++ src/LabelMaker/PrivacyPolicyTermsConditions.js
@@ -33,7 +33,7 @@ export default function PrivacyPolicyTermsConditions(props) {
 							<IconButton onClick={handleClose}><CloseIcon/></IconButton>
 						</Grid>
 						<Grid item xs={12}>
-							<p>Joanie built the Soap Labels app as a Free app. This SERVICE is provided by Joanie at no cost and is intended for use as is.</p>
+							<p>Joanie built the SoapLabels app as a Free app. This SERVICE is provided by Joanie at no cost and is intended for use as is.</p>
 							<p>This page is used to inform visitors regarding my policies with the collection, use, and disclosure of Personal Information if anyone decided to use my Service.</p>
 							<p>If you choose to use my Service, then you agree to the collection and use of information in relation to this policy. The Personal Information that I collect is used for providing and improving the Service. I will not use or share your information with anyone except as described in this Privacy Policy.</p>
 							<p>The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, which are accessible at Soap Labels unless otherwise defined in this Privacy Policy.</p>
